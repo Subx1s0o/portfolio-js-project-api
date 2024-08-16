792676e03a675cb7f8db09bccc5fc05096447a56
Subx1s0o <nazarsabardin@gmail.com>
2024-08-16 12:35:33 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://vita0609.github.io/TeamOfWinners/',
+    ],
+    methods: ['POST', 'GET'],
+  });
   await app.listen(5000);
 }
 bootstrap();
